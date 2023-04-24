c72708289e2a659a9b1fa276c67b923c19d521b4
mcccoleman <colemanmc02@gmail.com>
2023-04-24 09:17:41 -0400

    remove icon

--- src/components/SEO/SEO.tsx
+++ src/components/SEO/SEO.tsx
@@ -35,7 +35,7 @@ export const SEO: FC<SEOProps> = ({
       <meta name="image" content={seo.image} />
       <link
         rel="icon"
-        href="data:image/svg+xml,<svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 100 100'><text y='0.9em' font-size='90'>👤</text></svg>"
+        href="data:image/svg+xml,<svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 100 100'><text y='0.9em' font-size='90'></text></svg>"
       />
       {children}
     </>
