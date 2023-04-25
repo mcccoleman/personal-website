72f9c4a695783b277db99a409f887d15110893d4
Michael Coleman <37200650+mcccoleman@users.noreply.github.com>
2023-04-25 13:23:57 -0400

    update list (#4)

--- src/page-components/LanguagesLicensesAndSkills.tsx
+++ src/page-components/LanguagesLicensesAndSkills.tsx
@@ -1,10 +1,14 @@
 import React from "react";
 import { Flex } from "src/components/flex";
-import { languages, licensesAndSkills, constructList } from "./utils";
+import {
+  languagesAndTechnologies,
+  licensesAndSkills,
+  constructList,
+} from "./utils";
 
 export const LanguagesLicensesAndSkills = () => (
   <Flex justifyContent="space-between">
-    <Flex column>{constructList(languages)}</Flex>
+    <Flex column>{constructList(languagesAndTechnologies)}</Flex>
     <Flex column>{constructList(licensesAndSkills)}</Flex>
   </Flex>
 );
